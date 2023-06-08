76ed6f2263096b5d288f75e5b571c6db0bc71f86
MuhammadAmin04 <13muhammadamin0804@gmail.com>
2023-06-08 08:04:35 +0500

    m-2 lesson-14 js

---
+++ index.js
@@ -0,0 +1,64 @@
+"use strict";
+
+               // Obyekt
+
+// literal / new Object
+// assaign
+// JSON
+// class
+// function constructor
+// IIFE
+
+// const person={
+//  isName:"MuhammadAmin",
+//  age:19,
+//  hobby:function(){
+//   console.log("hhh");
+//  }
+ 
+// }
+// console.log(person['age']);
+// console.log(window);
+// console.log(window(origin));
+
+
+
+//set
+// const classRoom={
+
+// }
+
+// classRoom['name']='Netflix';
+// classRoom.size='large';
+// classRoom.info=function(){
+//    console.log("Room name: 'Netflix'");
+// }
+
+// console.log(classRoom);
+
+// const newObject=Object.assign({},{name:'whatsapp' , version:9.2});
+
+// console.log(newObject); 
+
+// var veg = { mushrooms: 30, peppers: 60, meatballs: 1, chicken: 2, olives: 4 };
+
+// function getKey(veg) {
+//   const arr = [];
+//   Object.keys(veg).forEach(function (item) {
+//     if (veg[item] > 5) arr.push(item);
+//   });
+//   return arr;
+// }
+
+// console.log(getKey(veg));
+
+
+
+
+
+
+
+
+
+
+
